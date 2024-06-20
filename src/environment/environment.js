8d48be251bb4ad7eba220471cd9868de280e48a5
kos15 <koustubhmishra91@gmail.com>
2024-06-20 13:43:36 +0530

    Updated environment file

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,4 +1,4 @@
 const ENV = {
-    API_KEY : 'AIzaSyCTSLaQkmAEoqctF06sVuzOYTQbJVvMy5k',
+    API_KEY : 'Put your API Keys here ,if not have one generate it from here https://ai.google.dev/gemini-api/docs/get-started/tutorial?lang=web#set-up-project',
 }
 export default ENV;
